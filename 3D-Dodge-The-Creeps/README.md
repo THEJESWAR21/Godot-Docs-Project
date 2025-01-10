349462eb3592679e57a38232d31a88398772cf91
THEJESWAR21 <thejeswar21jeeva@gmail.com>
2025-01-10 23:37:38 +0530

    Bug Fixed

---
+++ 3D-Dodge-The-Creeps/README.md
@@ -0,0 +1,5 @@
+# Dodge the Creeps 3D assets
+
+Used by the "Your first 3D game" tutorial:
+
+https://docs.godotengine.org/en/stable/getting_started/first_3d_game/index.html
--- 3D-Dodge-The-Creeps/Scenes/mob.gd
+++ 3D-Dodge-The-Creeps/Scenes/mob.gd
@@ -3,7 +3,7 @@ extends CharacterBody3D
 @export var min_speed = 10
 @export var max_speed = 18
 
-func _physcis_process(_delta):
+func _physics_process(_delta):
 	move_and_slide()
 
 func initialize(start_position, player_position):
@@ -12,6 +12,5 @@ func initialize(start_position, player_position):
 	var random_speed = randi_range(min_speed, max_speed)
 	velocity = Vector3.FORWARD * random_speed
 	velocity = velocity.rotated(Vector3.UP, rotation.y)
+
 	
-func _on_visibility_notifier_screen_exited() -> void:
-		queue_free()
